d282d27c764bc8ae33c766d2e96560cfe93d4841
araya <araya@araya.dev>
2022-08-11 16:11:57 +0900

    Fix wrong commands and comments

--- installer.sh
+++ installer.sh
@@ -7,16 +7,17 @@ brew install \
 	yarn \
 	peco \
 	ghq \
-	tig \
+	tig
 brew tap tkengo/highway && brew install highway
-# Vim setup
+
+# Setup Vim with plugins
 ## Install dein
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > /tmp/installer.sh
 sh /tmp/installer.sh ~/.cache/dein
 vim +"call dein#install()" +qall
 
 ## Setup Node.js
-# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
 ## Setup go
 curl https://raw.githubusercontent.com/arayaryoma/golang-tools-install-script/master/goinstall.sh | bash -s -- --version 1.18.5
