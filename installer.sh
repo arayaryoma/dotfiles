982e783d42b7b9b35f6112dc8ee64d88ef33fc43
araya <araya@arayas-MacBook-Air.local>
2023-06-17 17:32:25 +0900

    Add commands

--- installer.sh
+++ installer.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
 DEV_ROOT=$HOME/dev
+
+
+# Setup homebrew
+if ! command -v brew 1>/dev/null 2>&1; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+
+# Install packages with brew
 brew install \
-	tmux \
-	vim \
-	yarn \
-	peco \
-	ghq \
-	tig
+  	tmux \
+  	vim \
+  	yarn \
+  	peco \
+  	ghq \
+	git \
+  	tig
+
 brew tap tkengo/highway && brew install highway
 
 # Setup Vim with plugins
 ## Install dein
-curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > /tmp/installer.sh
+curl https://raw.githubusercontent.com/Shougo/dein-installer.vim/main/installer.sh > /tmp/installer.sh
 sh /tmp/installer.sh ~/.cache/dein
 vim +"call dein#install()" +qall
 
@@ -28,3 +39,9 @@ touch $HOME/.cache/shell/chpwd-recentdirs
 
 # Install Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# Install Rosetta
+softwareupdate --install-rosetta
+
+# Setup Node.js
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
