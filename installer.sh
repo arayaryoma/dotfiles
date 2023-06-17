97932cb2a69095f4c8af7e1f0730e128a23126b5
araya <araya@ragdoll.local>
2023-06-17 18:15:17 +0900

    Do not run installation scripts if already installed

--- installer.sh
+++ installer.sh
@@ -5,7 +5,8 @@ DEV_ROOT=$HOME/dev
 
 # Setup homebrew
 if ! command -v brew 1>/dev/null 2>&1; then
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo "brew is not installed"
+  # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 
@@ -22,29 +23,41 @@ brew install \
 brew tap tkengo/highway && brew install highway
 
 # Setup Vim with plugins
-## Install dein
-curl https://raw.githubusercontent.com/Shougo/dein-installer.vim/main/installer.sh > /tmp/installer.sh
-sh /tmp/installer.sh ~/.cache/dein
-vim +"call dein#install()" +qall
+DEIN_DIR="$HOME/.cache/dein"
 
-## Setup Node.js
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+if [ -d "$DEIN_DIR" ]; then
+    echo "dein.vim is already installed."
+else
+  echo "dein.vim is not installed."
+  ## Install dein
+  curl https://raw.githubusercontent.com/Shougo/dein-installer.vim/main/installer.sh > /tmp/installer.sh
+  sh /tmp/installer.sh $DEIN_DIR
+  vim +"call dein#install()" +qall   
+fi
+
+
 
 ## Setup go
-curl https://raw.githubusercontent.com/arayaryoma/golang-tools-install-script/master/goinstall.sh | bash -s -- --version 1.18.5
+if ! command -v go 1>/dev/null 2>&1; then
+  curl https://raw.githubusercontent.com/arayaryoma/golang-tools-install-script/master/goinstall.sh | bash -s -- --version 1.18.5
+fi
 
 ## Setup recentdirs cache
 mkdir -p $HOME/.cache/shell
 touch $HOME/.cache/shell/chpwd-recentdirs
 
 # Install Rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+if ! command -v rustup 1>/dev/null 2>&1; then
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+fi
 
 # Install Rosetta
-softwareupdate --install-rosetta
+softwareupdate --install-rosetta --agree-to-license
 
 # Setup Node.js
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+if ! command -v node 1>/dev/null 2>&1; then
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+fi
 
 # Install Docker
 if ! command -v docker 1>/dev/null 2>&1; then
